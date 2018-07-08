e47f336892c87e6d7c80fd04a8c348180acca3a6
tmiliszewski <tiagomiliszewski@gmail.com>
2018-07-08 15:58:46 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/tmiliszewski/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.tmiliszewski.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tmiliszewski.cursomc.domain.Cliente;
+import com.tmiliszewski.cursomc.repositories.ClienteRepository;
+import com.tmiliszewski.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado ! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+	}
+}
